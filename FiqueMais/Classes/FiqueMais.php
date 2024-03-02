09e2b3b913485c443bae035b0fb372fcba032cea
DaniBoy <79019339+DanielGoulartS@users.noreply.github.com>
2024-03-02 02:13:20 -0300

    Link de arrecadação e gatilho de contagem novo

--- FiqueMais/Classes/FiqueMais.php
+++ FiqueMais/Classes/FiqueMais.php
@@ -8,10 +8,21 @@
         <script src="./Scripts/pontos.js"></script>
     </head>
     <body>
+        <input type="checkbox" id="toggle">
+        <label for="toggle" id="balao">
+            <h2>Apoie-me</h2>
+            <ul>
+                <li><a href = "https://www.vakinha.com.br/4527939">Doe por Vakinha</a></li>
+                <li>ou Chave PIX:</li>
+                <li> <p id="pixKey"> 4527939@vakinha.com.br </p> </li>
+                <li><p id="arrow">«</p></li>
+                <br>
+            </ul>
+        </label>
         <form method="post">
             <audio loop id='aud'> <source src='./Media/SaySoInstrumental.mp3'></audio>
             <div id='div1'></div>
-            <div id='div2'>
+            <div id='div2' onclick="start()">
                 <a id='lol'>PLAY</a>
                 <div id='divcent'><h1 id='body'>00:00:00:00</h1></div>
                 <input type='text' id='nickname' class='box' name='nickname' placeholder='NickName' maxlength='20'></input>
--- FiqueMais/Classes/FiqueMaisCSS.php
+++ FiqueMais/Classes/FiqueMaisCSS.php
@@ -19,6 +19,70 @@
         position:relative;
         margin-top:5px;
     }
+    
+    #toggle{
+        display: none;
+    }
+    
+    #toggle:checked~#balao{
+        margin-left: -10%;
+    }
+    
+    #balao{
+        color:<?php echo 'rgb(' . rand(150, 255) . ',' . rand(150, 255) . ',' . rand(150, 255) . ')'; ?>;
+        text-shadow: 2px 0 #000,
+            -2px 0 #000,
+            0 2px #000,
+            0 -2px #000,
+            1px 1px #000,
+            -1px -1px #000,
+            1px -1px #000,
+            -1px 1px #000;
+        text-align: center;
+        border: 1px solid black;
+        z-index: 1;
+        position: fixed;
+        background-color: <?php echo 'rgba(' . rand(0, 100) . ',' . rand(50, 100) . ',' . rand(50, 100) . ',' . 0.8 . ')'; ?>;
+        margin: 10px;
+        aspect-ratio: 1 / 1;
+        transition: 0.5s;
+    }
+    
+    #balao h2{
+        font-size: 200%;
+        margin-top: 15%;
+    }
+    
+    #balao ul{
+        font-size: 90%;
+        color: lightgray;
+        list-style: none;
+        line-height: 20px;
+;
+        text-shadow: none;
+    }
+    ul a{
+        color: lightskyblue ;
+    }
+
+    #balao ul #pixKey{
+        color:<?php echo 'rgb(' . rand(150, 255) . ',' . rand(150, 255) . ',' . rand(150, 255) . ')'; ?>;
+        text-shadow: 2px 0 #000,
+            -2px 0 #000,
+            0 2px #000,
+            0 -2px #000,
+            1px 1px #000,
+            -1px -1px #000,
+            1px -1px #000,
+            -1px 1px #000;
+    }
+    
+    #balao ul #arrow{
+        color: black;
+        font-size: 200%;
+        margin-top:5%;
+    }
+
     #div1{
         width:100%;
         height: 100%;
@@ -88,6 +152,14 @@
         }
     }
     @media(max-width: 1000px){
+        #toggle:checked~#balao{
+            margin-left: -37%;
+        }
+
+        #balao{
+            margin-top: 120%;
+        }
+
         #divcent h1{
             font-size: 50%;
         }
